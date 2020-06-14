fb00696b78bff4f25ca22bf0f3dd3d664d3be9b2
vic huang <vic.huang1130x10@gmail.com>
2020-06-14 22:53:05 +0800

    update gitignore

---
+++ build/precache-manifest.87ccfecc433229074ad354d30557a413.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24b77da07ffe7e8f1eb7beb32dc7727f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00a6a6d04f94f5963a76",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "1a0b96115c1112af06b5",
+    "url": "/static/css/main.097b7562.chunk.css"
+  },
+  {
+    "revision": "00a6a6d04f94f5963a76",
+    "url": "/static/js/2.75be1e4b.chunk.js"
+  },
+  {
+    "revision": "88cbcb55f867c7e879dbc5e47becbf3b",
+    "url": "/static/js/2.75be1e4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a0b96115c1112af06b5",
+    "url": "/static/js/main.2a771e98.chunk.js"
+  },
+  {
+    "revision": "74d9420a3154859fff16",
+    "url": "/static/js/runtime-main.67483b21.js"
+  },
+  {
+    "revision": "fd29952eb9c82721f89be1f6eb8ea803",
+    "url": "/static/media/direction.fd29952e.png"
+  }
+]);
